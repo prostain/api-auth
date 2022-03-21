73897e26a77e9e3dc5f473c19261a81805c4fc57
peterson <peterson@ixcys.fr>
2022-03-21 12:40:39 +0100

    Brute-force protection

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const { Sequelize } = require('sequelize');
-const { sequelize, User, Role } = require('../models');
-const jwt = require('jsonwebtoken');
-var VerifyAccess = require('../services/verifyAccess.js');
-var EmailController = require('../services/emailServices');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const { Sequelize } = require("sequelize");
+const { sequelize, User, Role } = require("../models");
+const jwt = require("jsonwebtoken");
+var VerifyAccess = require("../services/verifyAccess.js");
+var EmailController = require("../services/emailServices");
+const nodemailer = require("nodemailer");
 
 const verifyAccess = new VerifyAccess();
 const emailController = new EmailController();
@@ -15,15 +15,65 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const { json } = require('body-parser');
+const bcrypt = require("bcrypt");
+const { json } = require("body-parser");
 //const { ne } = require('sequelize/dist/lib/operators');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-var randtoken = require('rand-token');
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+var randtoken = require("rand-token");
 
-require('dotenv').config();
+require("connect-flash");
+var ExpressBrute = require("express-brute"),
+  MemcachedStore = require("express-brute-memcached"),
+  moment = require("moment"),
+  store;
+
+//if (config.environment == "development") {
+store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+/*} else {
+  // stores state with memcached
+  store = new MemcachedStore(["127.0.0.1"], {
+    prefix: "NoConflicts",
+  });
+}*/
+
+var failCallback = function (req, res, next, nextValidRequestDate) {
+  res.status(401).send({
+    message:
+      "You've made too many failed attempts in a short period of time, please try again " +
+      moment(nextValidRequestDate).fromNow(),
+  }); // brute force protection triggered, send them back to the login page
+};
+var handleStoreError = function (error) {
+  log.error(error); // log this error so we can figure out what went wrong
+  // cause node to exit, hopefully restarting the process fixes the problem
+  throw {
+    message: error.message,
+    parent: error.parent,
+  };
+};
+// Start slowing requests after 5 failed attempts to do something for the same user
+var userBruteforce = new ExpressBrute(store, {
+  freeRetries: 1,
+  minWait: 5 * 60 * 1000, // 5 minutes
+  maxWait: 60 * 60 * 1000, // 1 hour,
+  failCallback: failCallback,
+  handleStoreError: handleStoreError,
+});
+// No more than 1000 login attempts per day per IP
+var globalBruteforce = new ExpressBrute(store, {
+  freeRetries: 1000,
+  attachResetToRequest: false,
+  refreshTimeoutOnRequest: false,
+  minWait: 25 * 60 * 60 * 1000, // 1 day 1 hour (should never reach this wait time)
+  maxWait: 25 * 60 * 60 * 1000, // 1 day 1 hour (should never reach this wait time)
+  lifetime: 24 * 60 * 60, // 1 day (seconds not milliseconds)
+  failCallback: failCallback,
+  handleStoreError: handleStoreError,
+});
+
+require("dotenv").config();
 
 /**
  * @swagger
@@ -75,55 +125,58 @@ require('dotenv').config();
  *       200:
  *         description: .
  */
-router.post('/register', async(req, res) => {
-    try {
-        var userToFind = await User.findOne({ where: { email: req.body.email } })
+router.post("/register", async (req, res) => {
+  try {
+    var userToFind = await User.findOne({ where: { email: req.body.email } });
 
-        if (userToFind) {
-            console.error("ERROR: Un compte avec cet email existe déjà");
-            return res.status(400).json({ error: "Un compte avec cet email existe déjà" });
-        }
-
-        var hashPWD = bcrypt.hashSync(req.body.password, 10);
-        var randPseudo = req.body.firstname + ~~(Math.random() * (9 - 1 + 1) + 1);
-        var user = {
-            pseudo: randPseudo,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            password: hashPWD,
-            address: req.body.address,
-            postalCode: req.body.postalCode,
-            city: req.body.city,
-            country: req.body.country,
-            isActive: true,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            roleId: 1
-        };
-        let user2 = await User.create(user)
-        let role = await Role.findOne({
-            where: { id: user.roleId }
-        })
-        user.id = user2.id;
-        user.role = {};
-        user.role.id = role.id;
-        user.role.name = role.name;
-
-        let accessToken = (await verifyAccess.generateAccessToken(user)).toString();
-        let refreshToken = (await verifyAccess.generateRefreshToken(user)).toString();
-
-        emailController.register(req.body.email);
-
-        res.send({
-            accessToken,
-            refreshToken,
-        });
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json(err)
+    if (userToFind) {
+      console.error("ERROR: Un compte avec cet email existe déjà");
+      return res
+        .status(400)
+        .json({ error: "Un compte avec cet email existe déjà" });
     }
 
+    var hashPWD = bcrypt.hashSync(req.body.password, 10);
+    var randPseudo = req.body.firstname + ~~(Math.random() * (9 - 1 + 1) + 1);
+    var user = {
+      pseudo: randPseudo,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashPWD,
+      address: req.body.address,
+      postalCode: req.body.postalCode,
+      city: req.body.city,
+      country: req.body.country,
+      isActive: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      roleId: 1,
+    };
+    let user2 = await User.create(user);
+    let role = await Role.findOne({
+      where: { id: user.roleId },
+    });
+    user.id = user2.id;
+    user.role = {};
+    user.role.id = role.id;
+    user.role.name = role.name;
+
+    let accessToken = (await verifyAccess.generateAccessToken(user)).toString();
+    let refreshToken = (
+      await verifyAccess.generateRefreshToken(user)
+    ).toString();
+
+    emailController.register(req.body.email);
+
+    res.send({
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 }); // Fin de la méthode register
 
 /**
@@ -153,58 +206,75 @@ router.post('/register', async(req, res) => {
  *         description: .
  */
 
-router.post('/login', async(req, res) => {
+router.post(
+  "/login",
+  globalBruteforce.prevent,
+  userBruteforce.getMiddleware({
+    key: function (req, res, next) {
+      // prevent too many attempts for the same username
+      next(req.body.username);
+    },
+  }),
+  async (req, res) => {
     try {
-        let email = req.body.email.trim();
-        const userToFind = await User.findOne({
-            where: { email: email },
-            include: [
-                { model: Role, as: 'role' }
-            ]
-        })
-        if (!userToFind) {
-            console.error("invalid credentials");
-            return res.status(400).json({ error: "invalid credentials" });
-        }
-        // TODO: fetch le user depuis la db basé sur l'email passé en paramètre
-        let enteredPassword = req.body.password;
-        let originalPassword = userToFind.password;
-        const correctPassword = await verifyAccess.comparePassword(enteredPassword, originalPassword);
+      let email = req.body.email.trim();
+      const userToFind = await User.findOne({
+        where: { email: email },
+        include: [{ model: Role, as: "role" }],
+      });
+      if (!userToFind) {
+        console.error("invalid credentials");
+        return res.status(400).json({ error: "invalid credentials" });
+      }
+      // TODO: fetch le user depuis la db basé sur l'email passé en paramètre
+      let enteredPassword = req.body.password;
+      let originalPassword = userToFind.password;
+      const correctPassword = await verifyAccess.comparePassword(
+        enteredPassword,
+        originalPassword
+      );
 
-        // TODO: check que le mot de passe du user est correct
-        if (!correctPassword) {
-            res.status(401).send('invalid credentials');
-            return;
-        }
-        let newUser = {
-            id: userToFind.id,
-            pseudo: userToFind.pseudo,
-            firstname: userToFind.firstname,
-            lastname: userToFind.lastname,
-            email: userToFind.email,
-            address: userToFind.address,
-            postalCode: userToFind.postalCode,
-            city: userToFind.city,
-            country: userToFind.country,
-            role: {
-                id: userToFind.role.id,
-                name: userToFind.role.name
-            }
-        };
+      // TODO: check que le mot de passe du user est correct
+      if (!correctPassword) {
+        res
+          .status(401)
+          .send("Aucun utilisateur trouvé pour cette adresse email");
+        return;
+      }
+      emailController.login(req.body.email);
 
-        let accessToken = (await verifyAccess.generateAccessToken(newUser)).toString();
-        let refreshToken = (await verifyAccess.generateRefreshToken(newUser)).toString();
-        res.send({
-            accessToken,
-            refreshToken,
-        });
+      let newUser = {
+        id: userToFind.id,
+        pseudo: userToFind.pseudo,
+        firstname: userToFind.firstname,
+        lastname: userToFind.lastname,
+        email: userToFind.email,
+        address: userToFind.address,
+        postalCode: userToFind.postalCode,
+        city: userToFind.city,
+        country: userToFind.country,
+        role: {
+          id: userToFind.role.id,
+          name: userToFind.role.name,
+        },
+      };
 
+      let accessToken = (
+        await verifyAccess.generateAccessToken(newUser)
+      ).toString();
+      let refreshToken = (
+        await verifyAccess.generateRefreshToken(newUser)
+      ).toString();
+      res.send({
+        accessToken,
+        refreshToken,
+      });
     } catch (err) {
-        console.log(err)
-        return res.status(500).json(err)
+      console.log(err);
+      return res.status(500).json(err);
     }
-
-}); // Fin de la méthode login
+  }
+); // Fin de la méthode login
 
 /**
  * @swagger
@@ -221,21 +291,26 @@ router.post('/login', async(req, res) => {
  *         description: .
  */
 
-router.post('/refreshToken', verifyAccess.authenticateRefreshToken, async(req, res) => {
+router.post(
+  "/refreshToken",
+  verifyAccess.authenticateRefreshToken,
+  async (req, res) => {
     try {
-        let user = req.user;
-        delete user.iat;
-        delete user.exp;
-        let accessToken = (await verifyAccess.generateAccessToken(user)).toString();
-        res.send({
-            accessToken: accessToken
-        });
-
+      let user = req.user;
+      delete user.iat;
+      delete user.exp;
+      let accessToken = (
+        await verifyAccess.generateAccessToken(user)
+      ).toString();
+      res.send({
+        accessToken: accessToken,
+      });
     } catch (err) {
-        console.log(err)
-        return res.status(500).json(err)
+      console.log(err);
+      return res.status(500).json(err);
     }
-});
+  }
+);
 
 /**
  * @swagger
@@ -260,46 +335,44 @@ router.post('/refreshToken', verifyAccess.authenticateRefreshToken, async(req, r
  *         description: .
  */
 
-router.post('/reset-password-email', async(req, res) => {
-    try {
-        var email = req.body.email;
+router.post("/reset-password-email", async (req, res) => {
+  try {
+    var email = req.body.email;
 
-        //console.log(sendEmail(email, fullUrl));
+    //console.log(sendEmail(email, fullUrl));
 
-        var userToFind = await User.findOne({ where: { email: req.body.email } })
+    var userToFind = await User.findOne({ where: { email: req.body.email } });
 
-        console.log(userToFind);
+    console.log(userToFind);
 
-        if (userToFind.email) {
+    if (userToFind.email) {
+      var token = randtoken.generate(20);
 
-            var token = randtoken.generate(20);
+      let sent = emailController.resetEmailPassword(req.body.email, token);
 
-            let sent = emailController.resetEmailPassword(req.body.email, token);
-
-            if (sent != '0') {
-
-                userToFind.resetPasswordToken = token.toString();
-                userToFind.save();
-
-            } else {
-                console.error("Something goes to wrong. Please try again");
-                return res.status(500).json({ error: 'Something goes to wrong. Please try again' });
-            }
-
-        } else {
-            console.error("invalid credentials");
-            return res.status(400).json({ error: "invalid credentials" });
-        }
-
-        res.send({
-            token
-        });
-        //return res.status(200).send({ message: 'Email envoyé avec succès'})
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json(err)
+      if (sent != "0") {
+        userToFind.resetPasswordToken = token.toString();
+        userToFind.save();
+      } else {
+        console.error("Something goes to wrong. Please try again");
+        return res
+          .status(500)
+          .json({ error: "Something goes to wrong. Please try again" });
+      }
+    } else {
+      console.error("invalid credentials");
+      return res.status(400).json({ error: "invalid credentials" });
     }
-})
+
+    res.send({
+      token,
+    });
+    //return res.status(200).send({ message: 'Email envoyé avec succès'})
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
 
 /**
  * @swagger
@@ -318,7 +391,7 @@ router.post('/reset-password-email', async(req, res) => {
  *               token:
  *                 type: string
  *                 description: .
- *                 example: 
+ *                 example:
  *               password:
  *                 type: string
  *                 description: .
@@ -327,41 +400,37 @@ router.post('/reset-password-email', async(req, res) => {
  *       200:
  *         description: .
  */
-router.post('/update-password', async(req, res) => {
-    try {
-        var token = req.body.token;
-        var password = req.body.password;
+router.post("/update-password", async (req, res) => {
+  try {
+    var token = req.body.token;
+    var password = req.body.password;
 
-        var userToFind = await User.findOne({ where: { resetPasswordToken: token } })
+    var userToFind = await User.findOne({
+      where: { resetPasswordToken: token },
+    });
 
+    if (userToFind) {
+      var saltRounds = 10;
 
-        if (userToFind) {
+      // var hash = bcrypt.hash(password, saltRounds);
 
-            var saltRounds = 10;
-
-            // var hash = bcrypt.hash(password, saltRounds);
-
-            bcrypt.genSalt(saltRounds, function(err, salt) {
-                bcrypt.hash(password, salt, function(err, hash) {
-
-                    userToFind.password = hash;
-                    userToFind.resetPasswordToken = null;
-                    userToFind.save();
-
-                });
-            });
-
-        } else {
-
-            console.error("invalid credentials");
-            return res.status(400).json({ error: "invalid credentials" });
-        }
-
-        res.status(200).json({ message: "mot de passe modifié avec succès" });
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json(err)
+      bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+          userToFind.password = hash;
+          userToFind.resetPasswordToken = null;
+          userToFind.save();
+        });
+      });
+    } else {
+      console.error("invalid credentials");
+      return res.status(400).json({ error: "invalid credentials" });
     }
-})
+
+    res.status(200).json({ message: "mot de passe modifié avec succès" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- services/emailServices.js
+++ services/emailServices.js
@@ -1,95 +1,101 @@
-'use strict';
+"use strict";
 
-const { Sequelize } = require('sequelize');
-const { sequelize, User } = require('../models');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
+const { Sequelize } = require("sequelize");
+const { sequelize, User } = require("../models");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 module.exports = class EmailController {
-   constructor() {
-   }
+  constructor() {}
 
-   register(to){
-try {
-
-    var object = 'Acount created';
-    var content = 'Merci d\'avoir créé votre compte chez 8 temps studio';
-    this.sendMyEmail(to, object, content);
-
-}catch(err){
-    console.log(err)
-    return res.status(500).json(err)
-}
-
-   }
-  
-   forgotPwd(to){
+  register(to) {
     try {
-        var object = 'Acount Récupération du mot de passe';
-        var content = 'aller sur ce lien pour réinitialiser votre mot de passe';
-        this.sendMyEmail(to, object, content);
-    
-    }catch(err){
-        console.log(err)
-        return res.status(500).json(err)
+      var object = "Acount created";
+      var content = "Merci d'avoir créé votre compte chez nous";
+      this.sendMyEmail(to, object, content);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
     }
-    
-     }
+  }
 
-
-     sendMyEmail(to, object, content){
-        try {
-        var transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,  //25,
-           // secure: false,
-            //service: 'gmail',
-            auth: {
-              user: process.env.GOOGLE_MAIL_ADRESS,
-              pass: process.env.GOOGLE_MAIL_KEY
-            },
-            tls: {
-              rejectUnauthorized: false
-          },
-          });
-        var mailOptions = {
-        from: 'noreplay@8-temps-studio.com', 
-          to: to, 
-         subject: object, 
-          text: content
-        };
-      
-        transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-    }catch(err){
-        console.log(err)
-        return res.status(500).json(err)
+  login(to) {
+    try {
+      var object = "New login";
+      var content =
+        "Une tentative de connexion a eu lieu sur votre compte à " +
+        new Date.now();
+      this.sendMyEmail(to, object, content);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
     }
-     }
+  }
 
-     resetEmailPassword(to, token){
-      try {
-      
-          var object = 'Reset Password Link - 8tempsstudio.com';
-          var content = 'You requested for reset password, kindly use this <a href="https://localhost:8080/reset-password?token=' + token + '">Reset password</a> to reset your password';
-          this.sendMyEmail(to, object, content);
-      
-      }catch(err){
-          console.log(err)
-          return res.status(500).json(err)
-      }
-     } 
-}
+  forgotPwd(to) {
+    try {
+      var object = "Acount Récupération du mot de passe";
+      var content = "aller sur ce lien pour réinitialiser votre mot de passe";
+      this.sendMyEmail(to, object, content);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+
+  sendMyEmail(to, object, content) {
+    try {
+      var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465, //25,
+        // secure: false,
+        //service: 'gmail',
+        auth: {
+          user: process.env.GOOGLE_MAIL_ADRESS,
+          pass: process.env.GOOGLE_MAIL_KEY,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
+      var mailOptions = {
+        from: "noreplay@example.com",
+        to: to,
+        subject: object,
+        text: content,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+
+  resetEmailPassword(to, token) {
+    try {
+      var object = "Reset Password Link - example.com";
+      var content =
+        'You requested for reset password, kindly use this <a href="https://localhost:8080/reset-password?token=' +
+        token +
+        '">Reset password</a> to reset your password';
+      this.sendMyEmail(to, object, content);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+};
