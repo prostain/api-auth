f253a64080d9d4cebf20a5eb1f68b9640919d5a1
peterson <peterson@ixcys.fr>
2022-03-22 10:42:12 +0100

    .

--- services/verifyAccess.js
+++ services/verifyAccess.js
@@ -12,121 +12,108 @@ const someOtherPlaintextPassword = "not_bacon";
 require("dotenv").config();
 
 module.exports = class VerifyAccess {
-    constructor() {}
+  constructor() {}
 
-    async generateAccessToken(user) {
-        return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: "1800s",
-        });
+  async generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "1800s",
+    });
+  }
+
+  async generateRefreshToken(user) {
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "1y",
+    });
+  }
+
+  async authenticateAdminToken(req, res, next) {
+    if (!req.headers.auth || !req.headers.auth.includes(" ")) {
+      return res.status(401).send("Demande non autorisée");
+    }
+    let token = req.headers.auth.split(" ")[1];
+    if (token === null) {
+      return res.status(401).send("Demande non autorisée");
+    }
+    let user = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET,
+      (err, user) => {
+        if (err) {
+          return (user = err.message);
+        } else {
+          return user;
+        }
+      }
+    );
+    if (typeof user === "string") {
+      return res.status(401).send(user);
     }
 
-    async generateRefreshToken(user) {
-        return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
-            expiresIn: "1y",
-        });
+    if (user.role.name !== "admin") return res.sendStatus(401);
+
+    req.user = user;
+    next();
+  }
+
+  async authenticateUserToken(req, res, next) {
+    if (!req.headers.auth || !req.headers.auth.includes(" ")) {
+      return res.status(401).send("Demande non autorisée");
+    }
+    let token = req.headers.auth.split(" ")[1];
+    if (token === null) {
+      return res.status(401).send("Demande non autorisée");
+    }
+    let user = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET,
+      (err, user) => {
+        if (err) {
+          return (user = err.message);
+        } else {
+          return user;
+        }
+      }
+    );
+    if (typeof user === "string") {
+      return res.status(401).send(user);
     }
 
-    async authenticateAdminToken(req, res, next) {
-        if (!req.headers.auth || !req.headers.auth.includes(" ")) {
-            return res.status(401).send("Demande non autorisée");
-        }
-        let token = req.headers.auth.split(" ")[1];
-        if (token === null) {
-            return res.status(401).send("Demande non autorisée");
-        }
-        let user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) {
-                return user = err.message;
-            } else {
-                return user
-            }
-        });
-        if (typeof user === 'string') {
-            return res.status(401).send(user);
-        }
+    req.user = user;
+    next();
+  }
 
-        if (user.role.name !== "admin") return res.sendStatus(401);
-
-        req.user = user;
-        next();
+  async authenticateRefreshToken(req, res, next) {
+    if (!req.headers.auth || !req.headers.auth.includes(" ")) {
+      return res.status(401).send("Demande non autorisée");
+    }
+    let token = req.headers.auth.split(" ")[1];
+    if (token === null) {
+      return res.status(401).send("Demande non autorisée");
+    }
+    let user = jwt.verify(
+      token,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, user) => {
+        if (err) {
+          return res.status(401).send(err.message);
+        } else {
+          return user;
+        }
+      }
+    );
+    if (!user) {
+      return res.status(401).send("Demande non autorisée");
     }
 
-    async authenticateUserToken(req, res, next) {
-        if (!req.headers.auth || !req.headers.auth.includes(" ")) {
-            return res.status(401).send("Demande non autorisée");
-        }
-        let token = req.headers.auth.split(" ")[1];
-        if (token === null) {
-            return res.status(401).send("Demande non autorisée");
-        }
-        let user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) {
-                return user = err.message;
-            } else {
-                return user
-            }
-        });
-        if (typeof user === 'string') {
-            return res.status(401).send(user);
-        }
+    req.user = user;
+    next();
+  }
 
-        req.user = user;
-        next();
-    }
+  async hashPassword(pwd) {
+    return bcrypt.hashSync(pwd, 10);
+  }
 
-    async authenticateVideoToken(req, res, next) {
-        if (!req.headers.token) {
-            return res.status(401).send('Demande non autorisée');
-        }
-        let token = req.query.token;
-        if (token === null) {
-            return res.status(401).send('Demande non autorisée');
-        }
-        let user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) {
-                return user = err.message;
-            } else {
-                return user
-            }
-        });
-        if (typeof user === 'string') {
-            return res.status(401).send(user);
-        }
-
-        req.user = user;
-        next();
-    }
-
-
-    async authenticateRefreshToken(req, res, next) {
-        if (!req.headers.auth || !req.headers.auth.includes(" ")) {
-            return res.status(401).send("Demande non autorisée");
-        }
-        let token = req.headers.auth.split(" ")[1];
-        if (token === null) {
-            return res.status(401).send("Demande non autorisée");
-        }
-        let user = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-            if (err) {
-                return res.status(401).send(err.message)
-            } else {
-                return user
-            }
-        });
-        if (!user) {
-            return res.status(401).send("Demande non autorisée");
-        }
-
-
-        req.user = user;
-        next();
-    }
-
-    async hashPassword(pwd) {
-        return bcrypt.hashSync(pwd, 10);
-    }
-
-    async comparePassword(enteredPassword, originalPassword) {
-        return await bcrypt.compare(enteredPassword, originalPassword);
-    }
+  async comparePassword(enteredPassword, originalPassword) {
+    return await bcrypt.compare(enteredPassword, originalPassword);
+  }
 };
